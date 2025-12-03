c3531eaf87d29b7bb8f1255742702557c5e3f372
chomusuke-png <owokenji@gmail.com>
2025-12-03 09:44:09 -0300

    aaaaaaaaaa

--- functions.php
+++ functions.php
@@ -220,6 +220,24 @@ function theme_register_sidebars() {
         'before_title' => '<h4>',
         'after_title' => '</h4>',
     ]);
+
+    register_sidebar([
+        'name' => 'Footer 2',
+        'id' => 'footer_2',
+        'before_widget' => '<div class="widget-item">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ]);
+
+    register_sidebar([
+        'name' => 'Footer 3',
+        'id' => 'footer_3',
+        'before_widget' => '<div class="widget-item">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ]);
 }
 add_action('widgets_init', 'theme_register_sidebars');
 
